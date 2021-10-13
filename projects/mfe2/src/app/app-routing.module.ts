abbc3bc123b3200129b5e931337e4ad81d1034ae
Felix <josefeliciano.huamanvalqui@nttdata.com>
2021-10-12 23:21:31 -0500

    Added auth module to mfe2

--- projects/mfe2/src/app/app-routing.module.ts
+++ projects/mfe2/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IdentityListComponent } from './identity/identity-list/identity-list.component';
+
+import { AutoLoginAllRoutesGuard } from 'angular-auth-oidc-client';
+import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [
-  { path: '', component: IdentityListComponent, pathMatch: 'full'}
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent, canActivate: [AutoLoginAllRoutesGuard] },
+  {
+    path: 'identity',
+    loadChildren: () => import('./identity/identity.module').then((m) => m.IdentityModule),
+    canLoad: [AutoLoginAllRoutesGuard],
+  }
 ];
 
 @NgModule({
--- projects/mfe2/src/app/app.module.ts
+++ projects/mfe2/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthConfigModule } from './auth/auth-config.module';
 import { IdentityModule } from './identity/identity.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     IdentityModule,
+    AuthConfigModule,
     AppRoutingModule
   ],
   providers: [],
