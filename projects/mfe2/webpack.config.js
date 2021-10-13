2f74fd5a1843ece8c51a40f552c0b9e3cc117e1a
Felix <josefeliciano.huamanvalqui@nttdata.com>
2021-10-13 15:10:38 -0500

    Comment out app module exporting

--- projects/mfe2/webpack.config.js
+++ projects/mfe2/webpack.config.js
@@ -28,12 +28,13 @@ module.exports = {
         filename: "remoteEntry.js",
         exposes: {
             './Module': './projects/mfe2/src/app/identity/identity.module.ts',
+            // './Module': './projects/mfe2/src/app/app.module.ts',
         }, 
 
         shared: {
           "@angular/core": { singleton: true, strictVersion: true }, 
-          "@angular/common": { singleton: true, strictVersion: true }, 
-          "@angular/common/http": { singleton: true, strictVersion: true }, 
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: '12.0.0'  }, 
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: '12.0.0'  }, 
           "@angular/router": { singleton: true, strictVersion: true },
           "angular-auth-oidc-client": { singleton: true, strictVersion: true },
 
