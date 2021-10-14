aa69e5cedde1040564d92778e74839cca25f737e
Felix <josefeliciano.huamanvalqui@nttdata.com>
2021-10-14 18:16:18 -0500

    Show identities in table

--- projects/mfe2/src/app/identity/identity-list/identity-list.component.ts
+++ projects/mfe2/src/app/identity/identity-list/identity-list.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { IdentitiesService, ListIdentitiesResponse } from '../services/identities.service';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DidJwk, IdentitiesService, ListIdentitiesResponse } from '../services/identities.service';
 
 @Component({
   selector: 'app-identity-list',
   templateUrl: './identity-list.component.html',
   styleUrls: ['./identity-list.component.scss']
 })
-export class IdentityListComponent implements OnInit {
+export class IdentityListComponent implements OnInit , AfterViewInit{
   result: ListIdentitiesResponse;
+  displayedColumns: string[] = ['did', 'date'];
+  dataSource = new MatTableDataSource<DidJwk>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(private indetityService: IdentitiesService) { }
 
   ngOnInit(): void {
     this.indetityService.getIdentities('').subscribe (result => {
       this.result = result;
+      this.dataSource.data = this.result.didJwk;
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.configureTable();
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  private configureTable(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
 }
--- projects/mfe2/src/app/identity/identity.module.ts
+++ projects/mfe2/src/app/identity/identity.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 import { IdentityRoutingModule } from './identity-routing.module';
 import { IdentityListComponent } from './identity-list/identity-list.component';
 
-
 @NgModule({
-  declarations: [
-    IdentityListComponent
-  ],
+  declarations: [IdentityListComponent],
   imports: [
     CommonModule,
-    IdentityRoutingModule
-  ]
+    IdentityRoutingModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ],
 })
-export class IdentityModule { }
+export class IdentityModule {}
